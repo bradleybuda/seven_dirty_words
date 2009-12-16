4ffb4ff7b5f33f35ba307d986119a05ddaf306c4
Bradley Buda <brad@tracksimple.com>
2009-12-15 23:15:21 -0800

    major refactor to be more "object-oriented"; not sure if i'm doing this correctly or not

--- src/seven_dirty_words.user.js
+++ src/seven_dirty_words.user.js
@@ -7,92 +7,120 @@
 // @resource       underscore http://documentcloud.github.com/underscore/underscore-min.js
 // ==/UserScript==
 
+// INIT
 // Inject jQuery and underscore into page (HACK)
 // From http://stackoverflow.com/questions/564342/jquery-ui-dialog-throw-errors-when-invoked-from-greasemonkey
-(function() {
-  var head = document.getElementsByTagName('head')[0];
+var head = document.getElementsByTagName('head')[0];
 
-  var jQueryScript = document.createElement('script');
-  jQueryScript.type = 'text/javascript';
-  var jQuery = GM_getResourceText('jQuery');
-  jQueryScript.innerHTML = jQuery;
-  head.appendChild(jQueryScript);
-  $ = unsafeWindow.$;
+var jQueryScript = document.createElement('script');
+jQueryScript.type = 'text/javascript';
+var jQuery = GM_getResourceText('jQuery');
+jQueryScript.innerHTML = jQuery;
+head.appendChild(jQueryScript);
+$ = unsafeWindow.$;
 
-  var underscoreScript = document.createElement('script');
-  underscoreScript.type = 'text/javascript';
-  var underscore = GM_getResourceText('underscore');
-  underscoreScript.innerHTML = underscore;
-  head.appendChild(underscoreScript);
-  _ = unsafeWindow._;
-})();
+var underscoreScript = document.createElement('script');
+underscoreScript.type = 'text/javascript';
+var underscore = GM_getResourceText('underscore');
+underscoreScript.innerHTML = underscore;
+head.appendChild(underscoreScript);
+_ = unsafeWindow._;
+// END INIT
 
-var nextColorIndex = 0;
-var availableColors = ['red', 'yellow', 'green', 'orange', 'blue'];
-function pickColor() {
-  return availableColors[nextColorIndex++ % availableColors.length];
+// begin 'class' here
+function SevenDirtyWords() {
+  this.colors = new SevenDirtyWords.Colors();
 }
 
-function log(o) {
+// "static" function - is this right?
+SevenDirtyWords.log = function(object){
   if (typeof console != "undefined")
-    console.log(o);
+    console.log(object);
+};
+
+// color functions as a nested class; this is a little crazy
+SevenDirtyWords.Colors = function() {
+  this.nextColorIndex = 0;
+  this.availableColors = ['red', 'yellow', 'green', 'orange', 'blue'];
 }
 
-log("About to scan lyrics");
+SevenDirtyWords.Colors.prototype.pickColor = function(){
+  return this.availableColors[this.nextColorIndex++ % this.availableColors.length];
+};
 
-// Seed dictionary with dirty words if necessary
-var initialWords = ['shit', 'piss', 'fuck', 'cunt', 'cocksucker', 'motherfucker', 'tits'];
-if (GM_getValue('dictionary', '__EMPTY__') == '__EMPTY__') {
-  log("initializing dictionary");
-  GM_setValue('dictionary', initialWords.join('|'));
-}
+// extract lyrics from document
+SevenDirtyWords.prototype.lyricsNodes = function(){
+  return $('#lyric_space *')
+    .contents()
+    .filter(function (){
+      return this.nodeType == 3;
+    });
+};
 
-// Load dictionary from local storage
-var dirtyWords = GM_getValue('dictionary','__EMPTY__').split('|');
-log("Dirty word list:");
-log(dirtyWords);
+// global state and scanning
+// TODO accessor functions for dirtyNodes
+SevenDirtyWords.prototype.dirtyNodes = new Object();
+SevenDirtyWords.prototype.scanLyrics = function(){
+  SevenDirtyWords.log("About to scan lyrics");
+  var lyricsNodes = this.lyricsNodes();
+  var wordList = this.wordList();
+  var dirtyNodes = this.dirtyNodes;
+  var colors = this.colors;
 
-// Find data to scan
-var lyricsNodes = $('#lyric_space *')
-  .contents()
-  .filter(function (){
-    return this.nodeType == 3;
-  });
+  $(lyricsNodes).each(function(){
+    var node = this;
 
-// scan the text
-var dirtyNodes = new Object();
-$(lyricsNodes).each(function(){
-  var node = this;
+    $(wordList).each(function(){
+      var dirtyWord = this;
+      var pat = dirtyWord.toUpperCase();
 
-  $(dirtyWords).each(function(){
-    var dirtyWord = this;
-    var pat = dirtyWord.toUpperCase();
-
-    // code below stolen from http://johannburkard.de/resources/Johann/jquery.highlight-3.js
-    var pos = node.data.toUpperCase().indexOf(pat);
-    if (pos >= 0) {
-      var spannode = document.createElement('span');
-      var middlebit = node.splitText(pos);
-      var endbit = middlebit.splitText(pat.length);
-      var middleclone = middlebit.cloneNode(true);
-      spannode.appendChild(middleclone);
-      middlebit.parentNode.replaceChild(spannode, middlebit);
-      if (typeof dirtyNodes[dirtyWord] == 'undefined') {
-        dirtyNodes[dirtyWord] = new Object();
-        dirtyNodes[dirtyWord].nodes = new Array();
-        dirtyNodes[dirtyWord].color = pickColor();
+      // code below stolen from http://johannburkard.de/resources/Johann/jquery.highlight-3.js
+      var pos = node.data.toUpperCase().indexOf(pat);
+      if (pos >= 0) {
+        var spannode = document.createElement('span');
+        var middlebit = node.splitText(pos);
+        var endbit = middlebit.splitText(pat.length);
+        var middleclone = middlebit.cloneNode(true);
+        spannode.appendChild(middleclone);
+        middlebit.parentNode.replaceChild(spannode, middlebit);
+        if (typeof dirtyNodes[dirtyWord] == 'undefined') {
+          dirtyNodes[dirtyWord] = new Object();
+          dirtyNodes[dirtyWord].nodes = new Array();
+          dirtyNodes[dirtyWord].color = colors.pickColor();
+        }
+        dirtyNodes[dirtyWord].nodes.push(spannode);
+        $(spannode).css('background-color', dirtyNodes[dirtyWord].color);
       }
-      dirtyNodes[dirtyWord].nodes.push(spannode);
-      $(spannode).css('background-color', dirtyNodes[dirtyWord].color);
-    }
+    });
   });
-});
 
-log("done scanning");
-log(dirtyNodes);
+  SevenDirtyWords.log("done scanning");
+  SevenDirtyWords.log(dirtyNodes);
+};
 
-// show a console with results
-var templateHtml = ' \
+// dictionary
+// for now, dictionary is never cached; it's always read out of prefs on each read/write
+SevenDirtyWords.prototype.wordList = function(){
+  // Seed dictionary with dirty words if necessary
+  var initialWords = ['shit', 'piss', 'fuck', 'cunt', 'cocksucker', 'motherfucker', 'tits'];
+  if (GM_getValue('dictionary', '__EMPTY__') == '__EMPTY__') {
+    SevenDirtyWords.log("initializing dictionary");
+    GM_setValue('dictionary', initialWords.join('|'));
+  }
+
+  // Load dictionary from local storage
+  return GM_getValue('dictionary','__EMPTY__').split('|');
+};
+
+SevenDirtyWords.prototype.addWord = function(word){
+  var existingWords = wordList();
+  // TODO check duplicates
+  existingWords.push(word);
+  GM_setValue('dictionary', existingWords.join('|'));
+};
+  
+// setup document
+SevenDirtyWords.prototype.templateHtml = ' \
 <div id="sevenDirtyWordsConsole"> \
   <h1>Dirty Words Found</h1> \
   <ul> \
@@ -120,61 +148,84 @@ var templateHtml = ' \
   </div> \
 </div>';
 
-$('body').append(_.template(templateHtml, {dirtyNodes: dirtyNodes, dirtyWords: dirtyWords}));
+SevenDirtyWords.prototype.clearExistingConsole = function(){
+  $('#sevenDirtyWordsConsole').remove();
+};
 
-// style the console
-$('#sevenDirtyWordsConsole').css({
-  'position': 'fixed',
-  'right': '5px',
-  'top': '5px',
-  'padding': '8px',
-  'width': '200px',
-  'background-color': 'white',
-  'border': '1px solid black'
-});
+SevenDirtyWords.prototype.renderHtml = function(){
+  // render template with current state
+  $('body').append(_.template(this.templateHtml, {dirtyNodes: this.dirtyNodes, dirtyWords: this.wordList()}));
+};
 
-$('#sevenDirtyWordsConsole ul').css({
-  'min-height': '30px'
-});
+SevenDirtyWords.prototype.applyStyles = function(){
+  $('#sevenDirtyWordsConsole').css({
+    'position': 'fixed',
+    'right': '5px',
+    'top': '5px',
+    'padding': '8px',
+    'width': '200px',
+    'background-color': 'white',
+    'border': '1px solid black'
+  });
 
-$('#editDictionaryBox').hide();
+  $('#sevenDirtyWordsConsole ul').css({
+    'min-height': '30px'
+  });
 
-$('#editDictionaryLink').css({
-  'height': '20px',
-  'position': 'absolute',
-  'bottom': '5px'
-});
+  $('#editDictionaryBox').hide();
 
-$('#editDictionaryLink a').css({
-  'align': 'right',
-  'color': 'blue'
-});
+  $('#editDictionaryLink').css({
+    'height': '20px',
+    'position': 'absolute',
+    'bottom': '5px'
+  });
 
-// add behaviors
-// expand edit dictionary
-$('#editDictionaryLink a').click(function(){
-  log("Opening dictionary for edit");
+  $('#editDictionaryLink a').css({
+    'align': 'right',
+    'color': 'blue'
+  });
+};
 
-  $('#editDictionaryBox').show();
-  $('#sevenDirtyWordsConsole').css('bottom', '5px'); // TODO animate that shiz
+SevenDirtyWords.prototype.addBehaviors = function(){
+  // expand edit dictionary
+  $('#editDictionaryLink a').click(function(){
+    SevenDirtyWords.log("Opening dictionary for edit");
+    
+    $('#editDictionaryBox').show();
+    $('#sevenDirtyWordsConsole').css('bottom', '5px'); // TODO animate that shiz
 
-  return false;
-});
+    return false;
+  });
 
-// press enter to add new word
-$('#newDictionaryWord input').keypress(function(event){
-  // TODO show the "how to use" instructions
+  // press enter to add new word
+  $('#newDictionaryWord input').keypress(function(event){
+    // TODO show the "how to use" instructions
+    
+    // ignore all keys other than the enter key
+    if (event.which != 13) {
+      return;
+    }
 
-  // ignore all keys other than the enter key
-  if (event.which != 13) {
-    return;
-  }
+    var newWord = $(this).val();
+    SevenDirtyWords.log("Adding " + newWord + " to dictionary");
+    // TODO persist
 
-  var newWord = $(this).val();
-  console.log("Adding " + newWord + " to dictionary");
-  // TODO persist
+    // TODO reload everything
 
-  // TODO reload everything
+    return false;
+  });
+};
 
-  return false;
-});
+SevenDirtyWords.prototype.render = function(){
+  this.clearExistingConsole();
+  this.renderHtml();
+  this.applyStyles();
+  this.addBehaviors();
+};
+
+// Run it!
+SevenDirtyWords.log("about to begin first scan");
+var instance = new SevenDirtyWords();
+instance.scanLyrics();
+instance.render();
+SevenDirtyWords.log("completed first scan");
